0c0e15ededab27c24d90f58a3beeeb753a5af207
Dani-Allwinson <daniallwinson27@gmail.com>
2021-07-02 12:15:49 +0530

    commit 5

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="David";
+names[0]="Yaakov";
 names[1]="John";
-names[2]="jenny";
-names[3]="Thomas";
+names[2]="Jen";
+names[3]="jason";
 names[4]="paul";
 names[5]="frank";
 names[6]="larry";
-names[7]="paul";
-names[8]="Jim";
-names[9]="jerry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
